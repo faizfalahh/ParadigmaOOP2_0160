fc657bd4a1dfcb7d2497faf380cf5afc47ebdd07
faizfalahh <faizfalah38@gmail.com>
2026-05-18 16:37:11 +0800

    menambahkan baseClass dan derivedClass dengan method perkenalan

---
+++ overriding.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() final{
+        cout << "Halo saya function dari base class";
+    }
+};
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Halo saya function dari derived Class";
+    }
+};
+int main() {
+    derivedClass a;
+    a.perkenalan1();
+
+    return 0;
+}
